c9590b14c49ba1884c1cd85205510101efdf57aa
Loniime <allaguirabia77@gmail.com>
2024-03-12 11:22:46 +0100

    ajout de composants

---
+++ src/app/components/bureaux/bureaux.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bureaux',
+  standalone: true,
+  imports: [],
+  templateUrl: './bureaux.component.html',
+  styleUrl: './bureaux.component.css'
+})
+export class BureauxComponent {
+
+}
