b9cfa4c72058a8559a062e896b89c6e7a98b0cd7
Loniime <allaguirabia77@gmail.com>
2024-03-12 11:24:09 +0100

    appel et import des composants dans app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SectionComponent} from './components/section/section.component';
+import {FormulaireComponent} from './components/formulaire/formulaire.component';
+import { AideComponent } from './components/aide/aide.component';
+import { BureauxComponent } from './components/bureaux/bureaux.component';
+import { AnnoncesComponent } from './components/annonces/annonces.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    SectionComponent,
+    FormulaireComponent,
+    AideComponent,
+    BureauxComponent,
+    AnnoncesComponent,
+    ContactComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
