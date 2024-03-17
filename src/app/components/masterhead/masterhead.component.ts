330d861de25e2aa3f42a642f641f9625da92dbaa
Loniime <allaguirabia77@gmail.com>
2024-03-17 02:41:35 +0100

    faire apparaitre la section declaration en cliquant sur c'est partie

--- src/app/components/masterhead/masterhead.component.ts
+++ src/app/components/masterhead/masterhead.component.ts
@@ -1,15 +1,20 @@
 import { Component, Directive, HostListener } from '@angular/core';
-
+import { SectionComponent } from '../section/section.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-masterhead',
   standalone: true,
-  imports: [],
+  imports: [SectionComponent,CommonModule],
   templateUrl: './masterhead.component.html',
   styleUrl: './masterhead.component.css'
 })
 export class MasterheadComponent {
 
+  showSection: boolean = false;
 
+  onGetStarted() {
+    this.showSection = true;
+  }
    
 
 }
