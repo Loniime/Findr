cf518bea2adcaddc8064ad49bb8f14a23f342c99
Loniime <allaguirabia77@gmail.com>
2024-03-17 02:40:32 +0100

    faire apparaitre le formulaire en cliquant sur declarer...

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FormulaireComponent } from '../formulaire/formulaire.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-section',
   standalone: true,
-  imports:[],
+  imports:[FormulaireComponent,CommonModule],
   templateUrl: './section.component.html',
   styleUrl: './section.component.css'
 })
 export class SectionComponent {
+  showSection: boolean = false;
 
+  onGetStarted() {
+    this.showSection = true;
+  }
 }
