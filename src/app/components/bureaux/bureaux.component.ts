ccf493742f7f74a22173916f276fcbf1165e1fd3
Loniime <allaguirabia77@gmail.com>
2024-03-17 15:37:49 +0100

    Enlever l'option random

--- src/app/components/bureaux/bureaux.component.ts
+++ src/app/components/bureaux/bureaux.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as data from '../../../assets/data.json';
-import { API } from '../../../assets/interfaceData';
+
 @Component({
   selector: 'app-bureaux',
   standalone: true,
@@ -10,27 +10,36 @@ import { API } from '../../../assets/interfaceData';
   styleUrl: './bureaux.component.css'
 })
 export class BureauxComponent {
-  jsonData= data.results;
+  jsonData = data.results;
+  uniqueNames: string[] = [];
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.setUniqueNames();
+    });
+  }
+
+  setUniqueNames() {
+    this.uniqueNames = this.getUniqueNames();
+    this.cdr.detectChanges();
+  }
 
   getUniqueNames(): string[] {
     const uniqueNames = new Set<string>();
 
     this.jsonData.forEach(item => {
-      if (item.ville !== null && item.ville !== undefined) { // Vérifier si la valeur est définie
+      if (item.ville !== null && item.ville !== undefined) {
         uniqueNames.add(item.ville);
       }
     });
 
-    return Array.from(uniqueNames);
+    return Array.from(uniqueNames).slice(0, 8);
   }
 
-  getRandomNames(): string[] {
-    const uniqueNames = this.getUniqueNames();
-
-    // Mélanger les noms aléatoirement
-    const shuffledNames = uniqueNames.sort(() => Math.random() - 0.5);
-
-    // Obtenir les 8 premiers noms aléatoires
+  /*getRandomNames(): string[] {
+    const shuffledNames = this.uniqueNames.sort(() => Math.random() - 0.5);
     return shuffledNames.slice(0, 8);
-  }
+  }*/
 }
