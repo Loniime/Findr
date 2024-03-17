b496ef78e00fb0c7c6b03908f5119e5fdcc5b480
Loniime <allaguirabia77@gmail.com>
2024-03-17 17:58:57 +0100

    update

--- src/app/components/bureaux/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-import { BureauxComponent } from "../bureaux.component";
-
-export const peopleRoutes : Routes = [
-    {
-        path: '',
-        component: BureauxComponent
-    }
-]
