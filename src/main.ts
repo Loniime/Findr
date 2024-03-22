6e03b8390148200500d593d8034aa49406615e58
Loniime <allaguirabia77@gmail.com>
2024-03-22 21:10:33 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { ApplicationRef, EnvironmentProviders, Provider } from '@angular/core';
+import { routes } from './app/routes';
+import { PreloadAllModules ,provideRouter, withDebugTracing, withPreloading } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
-
-bootstrapApplication(AppComponent,appConfig ).catch((err) => console.error(err));;
+const providers: Array<Provider | EnvironmentProviders> = [
+  provideRouter(routes , withDebugTracing(),withPreloading(PreloadAllModules)),
+  provideHttpClient()
+];
+const loadApplication = (application : ApplicationRef) => {
+console.info(application);
+};
+bootstrapApplication(AppComponent,{providers} ).then(item => loadApplication);
