8489c24ea86d410a93c1065a9e29ea95ba864034
Loniime <allaguirabia77@gmail.com>
2024-03-22 21:11:06 +0100

    ajout des components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {SectionComponent} from './components/section/section.component';
 import {FormulaireComponent} from './components/formulaire/formulaire.component';
 import { AideComponent } from './components/aide/aide.component';
-import { BureauxComponent } from './components/bureaux/bureaux.component';
+import { BureauxComponent} from './components/bureaux/bureaux.component';
 import { AnnoncesComponent } from './components/annonces/annonces.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { MasterheadComponent } from './components/masterhead/masterhead.component';
-import { CommonModule } from '@angular/common';
 import { RechercheComponent } from './components/recherche/recherche.component';
+import { SncfComponent } from './components/sncf/sncf.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -21,13 +21,12 @@ import { RechercheComponent } from './components/recherche/recherche.component';
     AnnoncesComponent,
     ContactComponent,
     MasterheadComponent,
-    CommonModule,
-    RechercheComponent
+    RechercheComponent,
+    SncfComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dashprojet';
-  
 }
